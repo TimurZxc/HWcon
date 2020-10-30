7760d6fa3139443787383c7e2f0146db46b013aa
Timur <72609967+TimurZxc@users.noreply.github.com>
2020-10-30 15:53:14 +0600

    Add files via upload

---
+++ algem.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 > nul
+
+
+
+cd "1 Kурс"\"1 Семестр"\"Алгебра и геометрия"
+
+echo > %DATE%.log
+type con > %DATE%.log
---
+++ op.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 > nul
+
+
+
+cd "1 Kурс"\"1 Семестр"\"Основы программирования"
+
+echo > %DATE%.log
+type con > %DATE%.log
